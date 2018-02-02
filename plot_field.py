dae658f91c8f46c6bceb864418f9bce5e349da1a
haykh <hayk.hakopyan@gmail.com>
2018-02-02 11:30:27 -0500

    fix

--- plot_field.py
+++ plot_field.py
@@ -42,18 +42,18 @@ start = 1
 end = max_number
 
 for step in tqdm(range(start, min(max_number, end))):
-	dens = getField(root, step, 'dens', getSizes(root, step))
+    dens = getField(root, step, 'dens', getSizes(root, step))
     densph = getField(root, step, 'densph', getSizes(root, step))
     denbw = getField(root, step, 'denbw', getSizes(root, step))
     multiplicity = divideArray(denbw, dens - denbw)
 
-	x = (np.arange(len(dens[0])) - max(np.arange(len(dens[0]))) * 0.5) * code_downsampling / skin_depth
-	y = (np.arange(len(dens)) - max(np.arange(len(dens))) * 0.5) * code_downsampling / skin_depth
-	x,y = np.meshgrid(x,y)
-	xmin = x.min()
-	xmax = x.max()
-	ymin = y.min()
-	ymax = y.max()
+    x = (np.arange(len(dens[0])) - max(np.arange(len(dens[0]))) * 0.5) * code_downsampling / skin_depth
+    y = (np.arange(len(dens)) - max(np.arange(len(dens))) * 0.5) * code_downsampling / skin_depth
+    x,y = np.meshgrid(x,y)
+    xmin = x.min()
+    xmax = x.max()
+    ymin = y.min()
+    ymax = y.max()
 
     fig = plt.figure(figsize=(25,15))
 
@@ -61,23 +61,23 @@ for step in tqdm(range(start, min(max_number, end))):
     ax2 = plt.subplot2grid((3,3),(1,0),colspan=3)
     ax3 = plt.subplot2grid((3,3),(2,0),colspan=3)
 
-	ax1 = plot_dens(ax1, x, y,
-					dens / ppc0, vmin = 1e-1, vmax = 5e3, label = r'plasma $[n_0]$',
-					xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax,
-					cmap = 'plasma',
-                    scaling = 'log', setover = 'red', extend = 'both')
+    ax1 = plot_dens(ax1, x, y,
+    dens / ppc0, vmin = 1e-1, vmax = 5e3, label = r'plasma $[n_0]$',
+    xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax,
+    cmap = 'plasma',
+    scaling = 'log', setover = 'red', extend = 'both')
     ax2 = plot_dens(ax2, x, y,
-                     densph, vmin = 10, vmax = 1e5, label = 'photons',
-                     xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax,
-                     cmap = 'afmhot',
-                     scaling = 'log', extend = 'both')
+    densph, vmin = 10, vmax = 1e5, label = 'photons',
+    xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax,
+    cmap = 'afmhot',
+    scaling = 'log', extend = 'both')
     ax3 = plot_dens(ax3, x, y,
-                     multiplicity, vmin = 1e-1, vmax = 100, label = 'pair-plasma multiplicity',
-                     xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax,
-                     cmap = 'Vega10',
-                     scaling = 'log', setover = 'white', extend = 'both',
+    multiplicity, vmin = 1e-1, vmax = 100, label = 'pair-plasma multiplicity',
+    xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax,
+    cmap = 'Vega10',
+    scaling = 'log', setover = 'white', extend = 'both',
 
-	plt.tight_layout()
-	plt.savefig(output_dir + "all_" + str(step).zfill(3) + ".png", dpi = 100)
+    plt.tight_layout()
+    plt.savefig(output_dir + "all_" + str(step).zfill(3) + ".png", dpi = 100)
 
-#plt.show()
+    #plt.show()
