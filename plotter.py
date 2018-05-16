6415664c26c1fe0ed7f7c55025b86e0be669581a
haykh <hayk.hakopyan@gmail.com>
2018-05-16 16:03:30 -0400

    sync

--- plotter.py
+++ plotter.py
@@ -181,7 +181,7 @@ def plot_photonB_vs_gamma(ax, root, step, sigma, gamma_c):
     if len(data['gam'].value) > 10000:
         cnt = ax.hist2d(np.log10(data['gam']), np.log10(data['B']), bins=(g_ax, b_ax), norm=mpl.colors.LogNorm(), cmap=my_cmap);
     else:
-        cnt = ax.hist2d([1], [-2], bins=(g_ax, b_ax), norm=mpl.colors.LogNorm(), cmap=my_cmap);
+        cnt = ax.hist2d([1], [-2], bins=(g_ax, b_ax), norm=mpl.colors.LogNorm(vmin=1, vmax=10), cmap=my_cmap);
     divider = make_axes_locatable(ax)
     cax = divider.append_axes("right", size="2%", pad=0.05)
     cbar = plt.colorbar(cnt[-1], cax = cax)
