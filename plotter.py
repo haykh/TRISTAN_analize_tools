a4fe9d4f2a6977e08b39d3b47dbd67de8ae658c1
haykh <hayk.hakopyan@gmail.com>
2018-05-16 15:20:59 -0400

    sync

--- plotter.py
+++ plotter.py
@@ -20,45 +20,6 @@ plt.register_cmap(name='inferno', cmap=inferno_cmap)
 global_fontsize = 15
 
 def plot_dens(ax, x, y,
-              dens, vmin, vmax,
-              label,
-              xmin, xmax, ymin, ymax,
-              cmap, scaling, setover = None, setunder = None, extend = 'neither', ret_cbar = False):
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="2%", pad=0.05)
-    my_cmap = copy.copy(mpl.cm.get_cmap(cmap))
-    if setunder is None:
-        my_cmap.set_bad(my_cmap(0))
-        my_cmap.set_under(my_cmap(0))
-    else:
-        my_cmap.set_bad(setunder)
-        my_cmap.set_under(setunder)
-    if setover is None:
-        my_cmap.set_over(my_cmap(255))
-    else:
-        my_cmap.set_over(setover)
-    if scaling == 'log':
-        strm = ax.pcolorfast(x, y, dens, cmap=my_cmap, norm=mpl.colors.LogNorm(vmin=vmin, vmax=vmax))
-    elif scaling == 'lin':
-        strm = ax.pcolorfast(x, y, dens, cmap=cmap, vmin=vmin, vmax=vmax)
-    elif scaling == 'symlog':
-        strm = ax.pcolorfast(x, y, dens, cmap=cmap, norm=mpl.colors.SymLogNorm(linthresh=vmax/10., vmin=vmin, vmax=vmax))
-    cbar = plt.colorbar(strm, cax = cax, extend=extend)
-    cbar.ax.yaxis.set_tick_params(pad=10)
-    ax.set_xlim(xmin, xmax)
-    ax.set_ylim(ymin, ymax)
-    # cbar.ax.tick_params(labelsize=fontsize)
-    ax.tick_params(axis='both')
-    ax.set_aspect(1)
-    ax.set_ylabel(r'$x$, [$c/\omega_{pl}$]')
-    props = dict(boxstyle='square', facecolor='white', alpha=0.9, edgecolor='none')
-    ax.text(0.02, 0.95, label, transform=ax.transAxes, verticalalignment='top', bbox=props)
-    if ret_cbar:
-        return (cbar, ax)
-    else:
-        return ax
-
-def plot_dens_new(ax, x, y,
                   dens, vmin, vmax,
                   label,
                   xmin, xmax, ymin, ymax,
@@ -103,52 +64,7 @@ def plot_dens_new(ax, x, y,
     else:
         return ax
 
-def plot_spectrum(ax, prtls, stride = 1,
-                  label = None, color = 'black', ls = '-',
-                  weights = None, min_e = 1e-1, max_e = 1e3, min_n = 1e0, max_n = 1e10, interp = False, normalize = False):
-
-    cnts, bns = np.histogram(prtls, bins=np.logspace(np.log10(min_e), np.log10(max_e), 150), weights = weights)
-    cnts = cnts * stride
-    bns = average(bns)
-    # g df(g) / dg
-
-    if normalize:
-        cnts = np.array(cnts)
-        cnts /= cnts.max()
-
-    if len(bns[cnts != 0]) < 5:
-        return ax
-
-    if interp:
-        # interpolation
-        cnts += 1
-        bns_new = np.logspace(np.log10(bns[0]), np.log10(bns[-1]), 1000)
-        spl = sp.interpolate.splrep(np.log10(bns), np.log10(cnts), s=np.log10(cnts).max() / 10.)
-        cnts_new = 10.0**(sp.interpolate.splev(np.log10(bns_new), spl))
-        ax.plot(bns_new, cnts_new, color = color, ls = ls, label = label, linewidth = 0.8)
-    else:
-        ax.step(bns, cnts, color = color, ls = ls, label = label, linewidth = 0.8)
-
-
-    ax.set_xscale('log')
-    ax.set_yscale('log', nonposy='clip')
-    ax.yaxis.tick_left()
-    ax.yaxis.set_label_position("left")
-
-    ax.legend(loc='upper center', ncol=5)
-    # ax.ticklabel_format(fontsize=fontsize)
-
-    ax.set_xlim(min_e, max_e)
-    ax.set_ylim(min_n, max_n)
-    ax.set_xlabel(r'$\varepsilon$, $[m_e c^2]$')
-    ax.set_ylabel(r'$\varepsilon~\mathrm{d}f(\varepsilon)/\mathrm{d}\varepsilon$')
-
-        # ax.plot([1e2,1e4], [1e9, 1e7], color='purple', ls='--')
-        # ax.text(2e3, 5e8, r'$\propto\gamma^{-1}$', fontsize=1.2*fontsize)
-    ax.tick_params(axis='both')
-    return ax
-
-def plot_spectrum_new(ax, bins, cnts, nprocs, bin_size = 151,
+def plot_spectrum(ax, bins, cnts, nprocs, bin_size = 151,
                       label = None, color = 'black', ls = '-', lw = 0.5, ncol = 3,
                       min_e = 1e-1, max_e = 1e3, min_n = 1e0, max_n = 1e10, normalize = False):
     def reduce_array(arr):
